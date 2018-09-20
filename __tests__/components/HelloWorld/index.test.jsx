1b2ca2efa7f91070e41bd78c7dbcc561caef9cef
Maurizio Battaghini <maurizio.battaghini@gmail.com>
2018-09-20 17:48:56 +0200

    fix test problem and update folders tree, update readme

--- __tests__/components/HelloWorld/index.test.jsx
+++ __tests__/components/HelloWorld/index.test.jsx
@@ -1,14 +1,40 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { mount, shallow } from 'enzyme';
+import toJson from 'enzyme-to-json';
 
 import HelloWorld from '../../../app/06.components/HelloWorld';
 
 describe('HelloWorld', () => {
-  test('should render a message from props', () => {
-    const message = 'Hello Boilerplate!';
-    const wrapper = shallow(<HelloWorld message={message} />);
-    expect(wrapper.find('.helloworld__message').get(0).props.children).toEqual(
-      message
-    );
+  describe('init component', () => {
+    test('shoul render the component', () => {
+      const wrapper = shallow(<HelloWorld />);
+      expect(wrapper).toHaveLength(1);
+    });
+
+    test('should match the snapshot', () => {
+      const wrapper = shallow(<HelloWorld />);
+      expect(toJson(wrapper)).toMatchSnapshot();
+    });
+  });
+
+  describe('message props', () => {
+    test('should render a message from props', () => {
+      const wrapper = mount(<HelloWorld />);
+      expect(wrapper.prop('message')).toBeDefined();
+    });
+
+    test('should render default message', () => {
+      const message = undefined;
+      const wrapper = mount(<HelloWorld message={message} />);
+      expect(wrapper.prop('message')).toEqual('Hello World!');
+      expect(toJson(wrapper)).toMatchSnapshot();
+    });
+
+    test('should render a specific message', () => {
+      const message = 'Hello Boilerplate!';
+      const wrapper = mount(<HelloWorld message={message} />);
+      expect(wrapper.prop('message')).toEqual(message);
+      expect(toJson(wrapper)).toMatchSnapshot();
+    });
   });
 });
